91ead3605772d9e664a24f6f8051f6a00f3a7120
Luis Salazar <111727302+LuisDakan@users.noreply.github.com>
2025-02-25 21:45:37 -0600

    linealización del grid

--- playMax.py
+++ playMax.py
@@ -22,10 +22,8 @@ class Minimax:
     #Jugador 
 
     def __init__(self):
-        for i in range(self.n):
-            self.grid.append([])
-            for j in range(self.n):
-                self.grid[i].append('_')
+        for i in range(self.n**2):
+                self.grid.append('_')
         self.symbol.append('o')
         self.symbol.append('x')
 
@@ -34,26 +32,26 @@ class Minimax:
     def show(self):
         for i in range(self.n):
             for j in range(self.n):
-                print(self.grid[i][j],end=" ")
+                print(self.grid[i*self.n+j],end=" ")
             print()
 
     #Funcion para marcar una casilla del grid
 
     def Mark(self,player,x,y):
-        self.grid[x][y]=self.symbol[player]
+        self.grid[x*self.n+y]=self.symbol[player]
         
     
     #Función para desmarcar una casilla del grid
 
     def unMark(self,player,x,y):
-        self.grid[x][y]='_'
+        self.grid[x*self.n+y]='_'
 
     #Función para determinar que el juego no puede continuar
 
     def gameOver(self):
         for i in range(self.n):
             for j in range(self.n):
-                if(self.grid[i][j]=='_'):
+                if(self.grid[i*self.n+j]=='_'):
                     return False
         return True
 
@@ -63,52 +61,53 @@ class Minimax:
         if(x>=0 and x<self.n and y>=0 and y<self.n):
             return True
         return False
+    
 
     def winner(self,player,x,y):
 
-        if(self.grid[x][y]!=self.symbol[player]):
+        if(self.grid[self.n*x+y]!=self.symbol[player]):
             return False
         
         #Checar vertical hacia adelante
-        if(self.validPoint(x+2,y) and self.grid[x+1][y]==self.grid[x+2][y] and self.grid[x+1][y]==self.symbol[player]):
+        if(self.validPoint(x+2,y) and self.grid[self.n*(x+1)+y]==self.grid[self.n*(x+2)+y] and self.grid[self.n*(x+1)+y]==self.symbol[player]):
             return True
         
         #Checar vertical en medio
-        if(self.validPoint(x+1,y) and self.validPoint(x-1,y) and self.grid[x+1][y]==self.grid[x-1][y] and self.grid[x+1][y]==self.symbol[player]):
+        if(self.validPoint(x+1,y) and self.validPoint(x-1,y) and self.grid[self.n*(x+1)+y]==self.grid[self.n*(x-1)+y] and self.grid[self.n*(x+1)+y]==self.symbol[player]):
             return True
 
         #vertical izquierda
-        if(self.validPoint(x-2,y) and self.grid[x-1][y]==self.grid[x-2][y] and self.grid[x-1][y]==self.symbol[player]):
+        if(self.validPoint(x-2,y) and self.grid[self.n*(x-1)+y]==self.grid[self.n*(x-2)+y] and self.grid[self.n*(x-1)+y]==self.symbol[player]):
             return True 
         
         #Horizontal hacia arriba
-        if(self.validPoint(x,y+2) and self.grid[x][y+1]==self.grid[x][y+2] and self.grid[x][y+1]==self.symbol[player]):
+        if(self.validPoint(x,y+2) and self.grid[self.n*x+y+1]==self.grid[self.n*x+y+2] and self.grid[self.n*x+y+1]==self.symbol[player]):
             return True
         
         #Horizontal hacia abajo
-        if(self.validPoint(x,y-2) and self.grid[x][y-1]==self.grid[x][y-2] and self.grid[x][y-1]==self.symbol[player]):
+        if(self.validPoint(x,y-2) and self.grid[self.n*x+y-1]==self.grid[self.n*x+y-2] and self.grid[self.n*x+y-1]==self.symbol[player]):
             return True
         
         #Horizontal en medio
-        if(self.validPoint(x,y+1) and self.validPoint(x,y-1) and self.grid[x][y+1]==self.grid[x][y-1] and self.grid[x][y+1]==self.symbol[player]):
+        if(self.validPoint(x,y+1) and self.validPoint(x,y-1) and self.grid[self.n*x+y+1]==self.grid[self.n*x+y-1] and self.grid[self.n*x+y+1]==self.symbol[player]):
             return True
         
         #Digonal positiva hacia arriba
-        if(self.validPoint(x-2,y+2) and self.grid[x-1][y+1]==self.grid[x-2][y+2] and self.grid[x-1][y+1]==self.symbol[player]):
+        if(self.validPoint(x-2,y+2) and self.grid[self.n*(x-1)+y+1]==self.grid[self.n*(x-2)+y+2] and self.grid[self.n*(x-1)+y+1]==self.symbol[player]):
             return True
         #Diagonal positiva hacia abajo
-        if(self.validPoint(x+2,y-2) and self.grid[x+1][y-1]==self.grid[x+2][y-2] and self.grid[x+1][y-1]==self.symbol[player]):
+        if(self.validPoint(x+2,y-2) and self.grid[self.n*(x+1)+y-1]==self.grid[self.n*(x+2)+y-2] and self.grid[self.n*(x+1)+y-1]==self.symbol[player]):
             return True
         #Diagonal positiva en medio
-        if(self.validPoint(x+1,y-1) and self.validPoint(x-1,y+1) and self.grid[x+1][y-1]==self.grid[x-1][y+1] and self.grid[x-1][y+1]==self.symbol[player]):
+        if(self.validPoint(x+1,y-1) and self.validPoint(x-1,y+1) and self.grid[self.n*(x+1)+y-1]==self.grid[self.n*(x-1)+y+1] and self.grid[self.n*(x-1)+y+1]==self.symbol[player]):
             return True
         #Diagonal negativa hacia arriba
-        if(self.validPoint(x+2,y+2) and self.grid[x+1][y+1]==self.grid[x+2][y+2] and self.grid[x+1][y+1]==self.symbol[player]):
+        if(self.validPoint(x+2,y+2) and self.grid[self.n*(x+1)+y+1]==self.grid[self.n*(x+2)+y+2] and self.grid[self.n*(x+1)+y+1]==self.symbol[player]):
             return True
         #Diagonal negativa hacia abajo
-        if(self.validPoint(x-2,y-2) and self.grid[x-2][y-2]==self.grid[x-1][y-1] and self.grid[x-1][y-1]==self.symbol[player]):
+        if(self.validPoint(x-2,y-2) and self.grid[self.n*(x-2)+y-2]==self.grid[self.n*(x-1)+y-1] and self.grid[self.n*(x-1)+y-1]==self.symbol[player]):
             return True
-        if(self.validPoint(x+1,y+1) and self.validPoint(x-1,y-1) and self.grid[x+1][y+1]==self.grid[x-1][y-1] and self.grid[x+1][y+1]==self.symbol[player]):
+        if(self.validPoint(x+1,y+1) and self.validPoint(x-1,y-1) and self.grid[self.n*(x+1)+y+1]==self.grid[self.n*(x-1)+y-1] and self.grid[self.n*(x+1)+y+1]==self.symbol[player]):
             return True
         
         return False
@@ -119,7 +118,7 @@ class Minimax:
         s=""
         for i in range(self.n):
             for j in range(self.n):
-                s+=self.grid[i][j]
+                s+=self.grid[self.n*i+j]
         return s
 
     #Algoritmo minimax
@@ -133,7 +132,7 @@ class Minimax:
             return self.dp[estado]
         for i in range(self.n):
             for j in range(self.n):
-                if(self.grid[i][j]=='_'):
+                if(self.grid[self.n*i+j]=='_'):
                     self.Mark(player,i,j)
                     if(self.winner(player,i,j)):
                         self.unMark(player,i,j)
@@ -163,7 +162,7 @@ class Minimax:
         coords=(0,0)
         for i in range(self.n):
             for j in range(self.n):
-                if(self.grid[i][j]=='_'):
+                if(self.grid[self.n*i+j]=='_'):
                     self.Mark(self.player,i,j)
                     if(self.winner(self.player,i,j)):
                         self.unMark(self.player,i,j)
